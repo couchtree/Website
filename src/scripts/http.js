927a305a925aa39b32f6a55810478480af3f7232
prooxeydev <louis1411@gmail.com>
2020-03-28 12:46:25 +0100

    Added language switch buttons

--- src/scripts/http.js
+++ src/scripts/http.js
@@ -1,23 +1,21 @@
 export default class Http {
+  constructor() {
+    this.xhr = new XMLHttpRequest()
+    this.setup()
+  }
 
-    constructor() {
-        this.xhr = new XMLHttpRequest()
-        this.setup()
+  setup() {
+    this.xhr.withCredentials = true
+  }
+
+  get(path) {
+    this.xhr.open("GET", path, false)
+    this.xhr.send()
+
+    if (this.xhr.readyState == 4 && this.xhr.status == 200) {
+      return this.xhr.responseText
     }
 
-    setup() {
-        this.xhr.withCredentials = true
-    }
-
-    get(path) {
-        this.xhr.open('GET', path, false)
-        this.xhr.send()
-
-        if (this.xhr.readyState == 4 && this.xhr.status == 200) {
-            return this.xhr.responseText
-        }
-
-        return ""
-    }
-
+    return ""
+  }
 }
